ba5b66d5f7ef5ed0f787d29eecae930bd66e284c
Micheldeboer <micheldb57@gmail.com>
2020-02-19 17:28:01 +0100

    Added notify for when a process is done (notify_process_done)

--- equestria/scripts/models.py
+++ equestria/scripts/models.py
@@ -2,12 +2,26 @@ from django.db import models
 from django.conf import settings
 import subprocess
 import os
+import threading
 
 
-def execute_script():
+def spawn_wait_thread():
     process = subprocess.Popen(['python3', os.path.join(settings.BASE_DIR, 'scripts/scriptje.py'), 'manage.py', 'bla'])
     Process.objects.create(process_id=process.pid, executing=True)
     print(process)
+    process.communicate()
+    notify_process_done(process)
+
+
+def notify_process_done(process):
+    print("Process is done!")
+
+
+# TODO we may need to pass some POST arguments later on.
+# (that is why this function looks so empty)
+def execute_script():
+    t1 = threading.Thread(target=spawn_wait_thread)
+    t1.start()
 
 
 class Process(models.Model):
