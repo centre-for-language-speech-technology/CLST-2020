0042db71347a4519b1275dbb85be15208c439ce6
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-02-19 15:12:52 +0100

    Added Process model

--- equestria/scripts/models.py
+++ equestria/scripts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Process(models.Model):
+
+    process_id = models.IntegerField()
+    input_file = models.FilePathField()
+    output_file = models.FilePathField()
+    executing = models.BooleanField()
