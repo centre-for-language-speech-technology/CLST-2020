aa7db52d7e1c8f371586a8d9396d52b2d068477d
Micheldeboer <micheldb57@gmail.com>
2020-02-19 17:01:25 +0100

    Used popen instead

--- equestria/scripts/models.py
+++ equestria/scripts/models.py
@@ -3,15 +3,15 @@ from django.conf import settings
 import subprocess
 import os
 
-# Create your models here.
 
 def execute_script():
-    subprocess.call(['python3', os.path.join(settings.BASE_DIR, 'scripts/scriptje.py'), 'manage.py', 'bla'])
+    process = subprocess.Popen(['python3', os.path.join(settings.BASE_DIR, 'scripts/scriptje.py'), 'manage.py', 'bla'])
+    Process.objects.create(process_id=process.pid, executing=True)
+    print(process)
+
 
 class Process(models.Model):
-
-    process_id = models.IntegerField()
+    process_id = models.IntegerField(blank=False)
     input_file = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
     output_file = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
-    executing = models.BooleanField()
-
+    executing = models.BooleanField(default=False)
