8d623708900279537fd34d7b11dc9aa97770418a
egidius <egidius@mysliwietz.de>
2020-02-22 07:00:11 -0800

    Added PraatScripts model and filled it with test data

--- equestria/fancybar/models.py
+++ equestria/fancybar/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class PraatScript(models.Model):
+    name = models.CharField(max_length=512)
+    # I haven't looked up how to reference local (server-side) files
+    path = models.FileField(default="", blank=True, upload_to='praat_scripts')
+    img  = models.ImageField(upload_to='praat_scripts_img', blank=True)
+    # If no image is provided, the icon is used instead
+    icon = models.CharField(max_length=4, blank=True)
+    desc = models.TextField(max_length=32768)
+
+    def __str__(self):
+        return self.name
