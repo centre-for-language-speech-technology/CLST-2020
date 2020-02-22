606a15e254873a5fd27c258e0a9a2b77cc2685ae
egidius <egidius@mysliwietz.de>
2020-02-22 08:07:21 -0800

    Media Images load now

--- equestria/equestria/urls.py
+++ equestria/equestria/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include(('fancybar.urls', 'fancybar'), namespace='fancybar')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
