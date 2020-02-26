5998d37df7dd5caedb0f43a82a1a6635058ae59a
egidius <egidius@mysliwietz.de>
2020-02-26 06:30:21 -0800

    Cleaned up views

--- equestria/fancybar/views.py
+++ equestria/fancybar/views.py
@@ -10,93 +10,55 @@ from fancybar.models import *
 # Create your views here.
 
 class GenericTemplate(TemplateView):
-        template_name = 'template.html'
+    template_name = 'template.html'
 
-        def get(self, request):
-                return render(request, self.template_name)
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        return render(request, self.template_name)
 
 
-        def post(self, request):
-                return render(request, self.template_name)
-
-        
 class Fancybar(GenericTemplate):
-        template_name = 'template.html'
+    template_name = 'template.html'
 
 
 class PraatScripts(TemplateView):
-        template_name = 'praat_scripts.html'
+    template_name = 'praat_scripts.html'
 
-        arg = {
-                'scripts': PraatScripts.objects.all(),
-        }
-        
-        def get(self, request):
-                return render(request, self.template_name, self.arg)
+    arg = {
+        'scripts': PraatScripts.objects.all(),
+    }
 
-        
-        def post(self, request):
-                name = request.POST.get("script_name", "")
-                print('"Running" script ' + name + "...")
-                self.arg['script_run'] = name
-                return render(request, self.template_name, self.arg)
+    def get(self, request):
+        return render(request, self.template_name, self.arg)
 
-class UploadWav(TemplateView):
-        template_name = 'upload_wav.html'
-
-        def get(self, request):
-                return render(request, self.template_name)
+    def post(self, request):
+        name = request.POST.get("script_name", "")
+        print('"Running" script ' + name + "...")
+        self.arg['script_run'] = name
+        return render(request, self.template_name, self.arg)
 
 
-        def post(self, request):
-                return render(request, self.template_name)
-
-class UploadTxt(TemplateView):
-        template_name = 'upload_txt.html'
-
-        def get(self, request):
-                return render(request, self.template_name)
+class UploadWav(GenericTemplate):
+    template_name = 'upload_wav.html'
 
 
-        def post(self, request):
-                return render(request, self.template_name)
-
-class ForcedAlignment(TemplateView):
-        template_name = 'forced_alignment.html'
-
-        def get(self, request):
-                return render(request, self.template_name)
+class UploadTxt(GenericTemplate):
+    template_name = 'upload_txt.html'
 
 
-        def post(self, request):
-                return render(request, self.template_name)
-
-class UpdateDictionary(TemplateView):
-        template_name = 'update_dictionary.html'
-
-        def get(self, request):
-                return render(request, self.template_name)
+class ForcedAlignment(GenericTemplate):
+    template_name = 'forced_alignment.html'
 
 
-        def post(self, request):
-                return render(request, self.template_name)
-
-class AutoSegmentation(TemplateView):
-        template_name = 'auto_segmentation.html'
-
-        def get(self, request):
-                return render(request, self.template_name)
+class UpdateDictionary(GenericTemplate):
+    template_name = 'update_dictionary.html'
 
 
-        def post(self, request):
-                return render(request, self.template_name)
-
-class DownloadResults(TemplateView):
-        template_name = 'download_results.html'
-
-        def get(self, request):
-                return render(request, self.template_name)
+class AutoSegmentation(GenericTemplate):
+    template_name = 'auto_segmentation.html'
 
 
-        def post(self, request):
-                return render(request, self.template_name)
+class DownloadResults(GenericTemplate):
+    template_name = 'download_results.html'
