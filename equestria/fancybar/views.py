40c7e8ce0b68ada9e4e63ecfdc644427938fe93c
THarri <martin.leger@outlook.de>
2020-02-26 14:10:37 +0100

    implemented upload from, faulty

--- equestria/fancybar/views.py
+++ equestria/fancybar/views.py
@@ -64,22 +64,22 @@ class UploadTxt(TemplateView):
 
         def post(self, request):
                 form = UploadFileForm(request.POST, request.FILES)
-		f = request.FILES['f']
-		fs = FileSystemStorage()
-		fs.save(f.name,f)
+                f = request.FILES['f']
+                fs = FileSystemStorage()
+                fs.save(f.name,f)
                 return render(request, self.template_name)
 
 
 class UploadView(TemplateView):
-	template_name = 'upload.html'
+        template_name = 'upload.html'
 
-	def get(self, request):
-		form = UploadFileForm()
-		return render(request, self.template_name, {'form': form})
+        def get(self, request):
+                form = UploadFileForm()
+                return render(request, self.template_name, {'form': form})
 
-	def post(self, request):
-		form = UploadFileForm(request.POST, request.FILES)
-		f = request.FILES['f']
-		fs = FileSystemStorage()
-		fs.save(f.name,f)
-		return render(request, self.template_name, {'form': form})
+        def post(self, request):
+                form = UploadFileForm(request.POST, request.FILES)
+                f = request.FILES['f']
+                fs = FileSystemStorage()
+                fs.save(f.name,f)
+                return render(request, self.template_name, {'form': form})
