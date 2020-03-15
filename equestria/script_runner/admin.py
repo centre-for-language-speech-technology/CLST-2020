61bd5b2332c068c39f9c12210dd81be4dbef1e74
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-03-15 16:14:18 +0100

    Black and pydoc fixes

--- equestria/script_runner/admin.py
+++ equestria/script_runner/admin.py
@@ -36,27 +36,38 @@ class ScriptAdmin(admin.ModelAdmin):
 
 
 class ProfileInline(admin.StackedInline):
+    """
+    Display profiles as a stacked inline form in the admin panel.
+
+    Show only profile objects that are explicitly created, do not show empty ones.
+    """
+
     model = models.Profile
     extra = 0
 
 
 class ProcessAdmin(admin.ModelAdmin):
-    """Process does not have inline fields (but may get them later)."""
+    """Profiles are displayed inline when creating/modifying processes."""
 
-    inlines = [
-        ProfileInline
-    ]
+    inlines = [ProfileInline]
 
 
 class InputTemplateInline(admin.StackedInline):
+    """
+    Display input templates as a stacked inline form in the admin panel.
+
+    Show only input template objects that are explicitly created, do not show empty ones.
+    """
+
     model = models.InputTemplate
     extra = 0
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    inlines = [
-        InputTemplateInline
-    ]
+    """Input templates are displayed inline when creating/modifying profiles."""
+
+    inlines = [InputTemplateInline]
+
 
 admin.site.register(models.Script, ScriptAdmin)
 admin.site.register(models.Process, ProcessAdmin)
