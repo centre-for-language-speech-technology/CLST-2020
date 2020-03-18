1969d14bf1d3b60aa03b23a34c7c22b77d180770
egidius <egidius@mysliwietz.de>
2020-03-18 17:01:40 +0100

    Live output working

--- equestria/script_runner/urls.py
+++ equestria/script_runner/urls.py
@@ -3,6 +3,11 @@ from script_runner.views import *
 
 urlpatterns = [
     path(
-        "process/<int:process_id>/", ProcessOverview.as_view(), name="process"
+        "process/<str:process>/output<path:p>",
+        CLAMFetch.as_view(),
+        name="clam_fetch",
     ),
+    path("process/<int:process_id>", ProcessOverview.as_view(), name="process"),
+    path("process/<int:process_id>", ProcessOverview.as_view(), name=""),
+    path("clam/<path:path>", Downloads.as_view(), name="clam"),
 ]
