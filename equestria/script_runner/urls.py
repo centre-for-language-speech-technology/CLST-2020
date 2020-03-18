30e4456d43c5215452b7f2053027b1ec5fcdaefe
egidius <egidius@mysliwietz.de>
2020-03-18 17:01:40 +0100

    Live output working

--- equestria/script_runner/urls.py
+++ equestria/script_runner/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from script_runner.views import *
 
 urlpatterns = [
+    path(
+        "process/<str:process>/output<path:p>",
+        CLAMFetch.as_view(),
+        name="clam_fetch",
+    ),
     path("process/<int:process>", ProcessOverview.as_view(), name="process"),
+    path("process/<int:process>", ProcessOverview.as_view(), name=""),
+    path("clam/<path:path>", Downloads.as_view(), name="clam"),
 ]
--- equestria/script_runner/views.py
+++ equestria/script_runner/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponseNotFound
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Process, Profile, InputTemplate
+from urllib.request import urlretrieve
+from os import makedirs
+from os.path import dirname, exists
+from django.views.static import serve
+from os.path import basename, dirname
 
 
 class ProcessOverview(TemplateView):
     template_name = "process_overview.html"
 
     def get(self, request, **kwargs):
-        key = kwargs.get('process')
+        key = kwargs.get("process")
         try:
             process = Process.objects.get(pk=key)
             arg = {"process": process}
@@ -22,5 +27,42 @@ class ProcessOverview(TemplateView):
             return render(request, self.template_name, arg)
         except Process.DoesNotExist:
             # TODO: Make a nice 404 page
-            return HttpResponseNotFound('<h1>Page not found</h1>')
+            return HttpResponseNotFound("<h1>Page not found</h1>")
 
+
+class CLAMFetch(TemplateView):
+    """
+    Download and serve files from CLAM
+
+    I initially did this by parsing XML, but this happens to be faster
+    """
+
+    def get(self, request, **kwargs):
+        clam_id = kwargs.get("process")
+        path = kwargs.get("p")
+        process = Process.objects.get(clam_id=clam_id)
+
+        save_file = "outputs/{}{}".format(clam_id, path)
+        print(save_file)
+        print("{}/{}/output{}".format(process.script.hostname, clam_id, path))
+        if not exists(dirname(save_file)):
+            makedirs(dirname(save_file))
+        urlretrieve(
+            "{}/{}/output{}".format(process.script.hostname, clam_id, path),
+            save_file,
+        )
+        return redirect(
+            "script_runner:clam", path="outputs/{}/{}".format(clam_id, path),
+        )
+
+
+class Downloads(TemplateView):
+    """View to serve downloadable files."""
+
+    def get(self, request, path):
+        """Respond to get request by serving requested download file."""
+        return serve(request, basename(path), dirname(path))
+
+    def post(self, request, path):
+        """Respond to post request by serving requested download file."""
+        return serve(request, basename(path), dirname(path))
