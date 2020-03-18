9c745ac75e5dd0d2ea21041ea02c96dfa84884d4
MaJoEi <matthias.eisenlohr@web.de>
2020-03-18 16:53:06 +0100

    Views are now restricted to logged-in users

--- equestria/upload/views.py
+++ equestria/upload/views.py
@@ -5,51 +5,65 @@ from django.views.generic import TemplateView
 from .forms import UploadTXTForm, UploadWAVForm
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 class UploadWAVView(TemplateView):
     template_name = 'upload_wav2.html'
 
     def get(self, request):
-        form = UploadWAVForm()
-        return render(request, self.template_name, {'WAVform': form})
+        if not request.user.is_authenticated:
+            return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        else:
+            form = UploadWAVForm()
+            return render(request, self.template_name, {'WAVform': form})
 
     def post(self, request):
-        form = UploadWAVForm(request.POST, request.FILES)
-        if form.is_valid():
-            print ('valid form')
-            wavfile = request.FILES['wavFile']
-            fs = FileSystemStorage(location='media/sname/wav')
-            if fs.exists(wavfile.name):
-                os.remove(os.path.join(
-                    settings.MEDIA_ROOT + '/sname/wav', wavfile.name))
-            fs.save(wavfile.name, wavfile)
+        if not request.user.is_authenticated:
+            return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
         else:
-            print ('invalid form')
-            print (form.errors)
-            # return error to AJAX function to print
-        return HttpResponseRedirect('/forced/')
+            form = UploadWAVForm(request.POST, request.FILES)
+            if form.is_valid():
+                print ('valid form')
+                wavfile = request.FILES['wavFile']
+                fs = FileSystemStorage(location='media/sname/wav')
+                if fs.exists(wavfile.name):
+                    os.remove(os.path.join(
+                        settings.MEDIA_ROOT + '/sname/wav', wavfile.name))
+                fs.save(wavfile.name, wavfile)
+            else:
+                print ('invalid form')
+                print (form.errors)
+                # return error to AJAX function to print
+            return HttpResponseRedirect('/forced/')
 
 
 class UploadTXTView(TemplateView):
     template_name = 'upload_txt2.html'
 
     def get(self, request):
-        form = UploadTXTForm()
-        return render(request, self.template_name, {'TXTform': form})
+        if not request.user.is_authenticated:
+            return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        else:
+            form = UploadTXTForm()
+            return render(request, self.template_name, {'TXTform': form})
 
     def post(self, request):
-        form = UploadTXTForm(request.POST, request.FILES)
-        if form.is_valid():
-            print ('valid form')
-            txtfile = request.FILES['txtFile']
-            fs = FileSystemStorage(location='media/sname/txt')
-            if fs.exists(txtfile.name):
-                os.remove(os.path.join(
-                    settings.MEDIA_ROOT + '/sname/txt', txtfile.name))
-            fs.save(txtfile.name, txtfile)
+        if not request.user.is_authenticated:
+            return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
         else:
-            print ('invalid form')
-            print (form.errors)
-            #return error to AJAX function to print
-        return HttpResponseRedirect('/forced/')
+            form = UploadTXTForm(request.POST, request.FILES)
+            if form.is_valid():
+                print ('valid form')
+                txtfile = request.FILES['txtFile']
+                fs = FileSystemStorage(location='media/sname/txt')
+                if fs.exists(txtfile.name):
+                    os.remove(os.path.join(
+                        settings.MEDIA_ROOT + '/sname/txt', txtfile.name))
+                fs.save(txtfile.name, txtfile)
+            else:
+                print ('invalid form')
+                print (form.errors)
+                #return error to AJAX function to print
+            return HttpResponseRedirect('/forced/')
