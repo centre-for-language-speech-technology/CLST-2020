e9e2aac076371afe2920f6427bb71bc876569f00
egidius <egidius@mysliwietz.de>
2020-03-18 17:01:40 +0100

    Live output working

--- equestria/script_runner/views.py
+++ equestria/script_runner/views.py
@@ -4,7 +4,13 @@ import clam.common.status
 from django.http import HttpResponseNotFound
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
-from .models import Process, Profile, InputTemplate, Script
+
+from .models import Process, Profile, InputTemplate
+from urllib.request import urlretrieve
+from os import makedirs
+from os.path import exists
+from django.views.static import serve
+from os.path import basename, dirname
 
 
 class ProcessOverview(TemplateView):
@@ -85,3 +91,42 @@ class ProcessOverview(TemplateView):
             clamclient.addinputfile(process_to_run.clam_id, template, file)
 
         clamclient.startsafe(process_to_run.clam_id)
+
+
+class CLAMFetch(TemplateView):
+    """
+    Download and serve files from CLAM
+
+    I initially did this by parsing XML, but this happens to be faster
+    """
+
+    def get(self, request, **kwargs):
+        clam_id = kwargs.get("process")
+        path = kwargs.get("p")
+        process = Process.objects.get(clam_id=clam_id)
+
+        save_file = "outputs/{}{}".format(clam_id, path)
+        print(save_file)
+        print("{}/{}/output{}".format(process.script.hostname, clam_id, path))
+        if not exists(dirname(save_file)):
+            makedirs(dirname(save_file))
+        urlretrieve(
+            "{}/{}/output{}".format(process.script.hostname, clam_id, path),
+            save_file,
+        )
+        return redirect(
+            "script_runner:clam", path="outputs/{}/{}".format(clam_id, path),
+        )
+
+
+class Downloads(TemplateView):
+    """View to serve downloadable files."""
+
+    def get(self, request, path):
+        """Respond to get request by serving requested download file."""
+        return serve(request, basename(path), dirname(path))
+
+    def post(self, request, path):
+        """Respond to post request by serving requested download file."""
+        return serve(request, basename(path), dirname(path))
+
