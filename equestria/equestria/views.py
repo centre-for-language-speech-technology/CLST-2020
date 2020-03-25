49fa6f8e31717ae3df4dee436dc8d2d823ff6e7d
Egidius Mysliwietz <egidius@mysliwietz.de>
2020-03-25 15:26:34 +0100

    Removing fancybar

--- equestria/equestria/views.py
+++ equestria/equestria/views.py
@@ -45,35 +45,6 @@ class PraatScripts(RestrictedTemplate):
     template_name = "praat_scripts.html"
 
 
-class ForcedAlignment(TemplateView):
-    """Page to run main forced alignment script. Contains two upload forms for txt and wav files."""
-
-    template_name = "forced_alignment.html"
-    arg = dict()
-
-    def __init__(self, **kwargs):
-        """Load all script from the database."""
-        super().__init__(**kwargs)
-        self.arg["scripts"] = Script.objects.select_related().filter(
-            forced_alignment_script=True
-        )
-
-    def get(self, request):
-        """Responds to get request and loads upload forms."""
-        if not request.user.is_authenticated:
-            return redirect("%s?next=%s" % (settings.LOGIN_URL, request.path))
-        else:
-            form = uploadForms.UploadFileForm()
-            return render(request, self.template_name, self.arg)
-
-    def post(self, request):
-        """Save uploaded files. TODO: Does not yet run anything."""
-        if not request.user.is_authenticated:
-            return redirect("%s?next=%s" % (settings.LOGIN_URL, request.path))
-        else:
-            return render(request, self.template_name, self.arg)
-
-
 class UpdateDictionary(RestrictedTemplate):
     """Page to update dictionary."""
 
