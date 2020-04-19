ac8e870c5c33b359a548c6439a9866de765dc6d0
tharrry <martin.leger@outlook.de>
2020-04-19 11:42:48 +0200

    deleted deadend

--- equestria/upload/urls.py
+++ equestria/upload/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import UploadWAVView, UploadProjectView
+from .views import UploadProjectView
 
 urlpatterns = [
-    path("wav", UploadWAVView.as_view(), name="uploadwav_form"),
-    # path("txt", UploadTXTView.as_view(), name="uploadtxt_form"),
     path(
         "<int:project_id>", UploadProjectView.as_view(), name="upload_project"
     ),
