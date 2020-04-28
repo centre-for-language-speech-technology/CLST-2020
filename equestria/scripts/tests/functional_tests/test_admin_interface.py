49b89e66597dadb04d28f06c3c2f7cb43d49f859
Egidius Mysliwietz <egidius@mysliwietz.de>
2020-04-28 21:48:05 +0200

    Started with admin tests

--- equestria/scripts/tests/functional_tests/test_admin_interface.py
+++ equestria/scripts/tests/functional_tests/test_admin_interface.py
@@ -4,7 +4,7 @@ from equestria.tests.functional_tests.meta import GenericFuncTest
 class TestAdminInterface(GenericFuncTest):
     """Tests basic UI elements."""
 
-    fixtures = ["simple_pipelines"]
+    fixtures = ["superuser"]
 
     def test_admin_interface(self):
         """Test if user is greeted by welcome text."""
