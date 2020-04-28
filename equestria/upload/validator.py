f0ea3bcf35b6bde8e7dd8e68691676aa329aa0cb
ValentijnAlbertus <47530656+ValentijnAlbertus@users.noreply.github.com>
2020-04-28 18:42:27 +0200

    Pydoc fix

--- equestria/upload/validator.py
+++ equestria/upload/validator.py
@@ -2,9 +2,15 @@ import os
 from django.core.exceptions import ValidationError
 
 
-def validate_file_extension(value):
-    # Ensures that files with other extensions cannot be uploaded
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+def validate_file_extension(file):
+    """
+    Checks if the file extension is in the whitelist. 
+
+    :param file: the file to be validated
+    :param file: the file to be uploaded
+    :return: None
+    """
+    ext = os.path.splitext(file.name)[1]  # [0] returns path+filename
     valid_extensions = [".wav", ".txt", ".tg", ".zip"]
     if not ext.lower() in valid_extensions:
         raise ValidationError("Unsupported file extension.")
