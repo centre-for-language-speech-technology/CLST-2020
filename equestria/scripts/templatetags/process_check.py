9de817041861da9fb1a5b553316b3b8c47ab49da
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-05-04 12:27:28 +0200

    Removed migrations and finished code

--- equestria/scripts/templatetags/process_check.py
+++ equestria/scripts/templatetags/process_check.py
@@ -5,13 +5,21 @@ register = template.Library()
 
 @register.simple_tag
 def check_project(**kwargs):
-    project = kwargs.get("project")
-    if project.can_start_new_process():
-        if project.has_empty_extension_file([".oov"]):
-            return "Finished"
-    elif project.current_process is not None:
-        if project.current_process_script == project.pipeline.fa_script:
-            return "FA"
-        elif project.current_process_script == project.pipeline.g2p_script:
-            return "G2P"
+    """
+    Check the status of a current project.
+
+    :param kwargs: keyword arguments
+    :return: a string, FA if the project is currently running an FA script, G2P if the project is currently running a
+    G2P script, Finished if the project is currently finished running FA and None if the project has not started FA yet
+    """
+    project = kwargs.get("project", None)
+    if project is not None:
+        if project.current_process is not None:
+            if project.current_process == project.pipeline.fa_script:
+                return "FA"
+            elif project.current_process == project.pipeline.g2p_script:
+                return "G2P"
+        else:
+            if project.finished_fa():
+                return "Finished"
     return "None"
