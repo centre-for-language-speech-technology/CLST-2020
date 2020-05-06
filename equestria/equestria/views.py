7f245275a75cc184e3556b5b6d4493a88770692e
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-05-06 19:57:22 +0200

    Added new header to views

--- equestria/equestria/views.py
+++ equestria/equestria/views.py
@@ -1,26 +1,7 @@
 from equestria.view_generic import *
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class WelcomePage(GenericTemplate):
     """Page to greet the user."""
 
     template_name = "equestria/welcome.html"
-
-
-class PraatScripts(RestrictedTemplate):
-    """Page to list all non-primary (praat) scripts."""
-
-    template_name = "equestria/praat_scripts.html"
-
-
-class UpdateDictionary(RestrictedTemplate):
-    """Page to update dictionary."""
-
-    template_name = "equestria/update_dictionary.html"
-
-
-class AutoSegmentation(RestrictedTemplate):
-    """Page to run auto-segmentation."""
-
-    template_name = "equestria/auto_segmentation.html"
