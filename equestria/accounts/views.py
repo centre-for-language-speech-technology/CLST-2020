95bde1d2e534c309e722e2fc1e8791f2415122f4
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-05-06 16:21:40 +0200

    Added pipeline css

--- equestria/accounts/views.py
+++ equestria/accounts/views.py
@@ -6,7 +6,6 @@ from django.views.generic import TemplateView
 from django.contrib import messages
 from accounts.models import UserProfile
 from upload.forms import UploadForm
-from .forms import AudioSelectForm
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 
